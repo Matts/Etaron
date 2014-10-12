e2f6eb91852c5eb5b0380f2f7b20258833df0dfb
Trevi Awater <AwaterTrevi@Gmail.com>
2014-10-12 16:08:31 +0200

    New database structure

--- et-server/src/main/java/nl/dalthow/etaron/db/Query.java
+++ et-server/src/main/java/nl/dalthow/etaron/db/Query.java
@@ -11,10 +11,10 @@ package nl.dalthow.etaron.db;
 
 public class Query 
 {
-	// Query Declaration
-	
-    public static final String USER_SEARCH = "SELECT\n" + "    cr.Username, cr.UserID id, cr.Email, st.Music, st.Sound\n" + "FROM\n" + "    Credentials cr left outer join Settings st\n" + "ON\n" + "    cr.UserID = st.UserID\n" + "WHERE\n" + "    cr.Username = :username and\n" + "    cr.Password = :password";
-    public static final String SCORE_SEARCH = "select \n" + "    max(sc.Score) value, sc.Level\n" + "from\n" + "    Scores sc\n" + "WHERE\n" + "    sc.UserID = :userId\n" + "group by sc.Level";
-    public static final String UPDATE_SETTINGS = "insert into Settings (UserID, Music, Sound) values (:userId, :music, :sound) on duplicate key update Music=values(Music), Sound=values(Sound)";
-	public static final String UPDATE_SCORES = "insert into Scores (UserID, Level, Score, Date) values (:userId, :level, :score, :date)";
+	// Query declaration
+
+	public static final String USER_SEARCH = "SELECT users.id, users.username, users.email, settings.music, settings.sound \n FROM users \n LEFT OUTER JOIN settings \n ON users.id = settings.usersId \n WHERE users.username = :username AND   users.password = :password";
+    public static final String SCORE_SEARCH = "SELECT max(scores.score) AS 'score', scores.level \n FROM scores \n WHERE scores.usersId = :userId \n GROUP BY scores.level";
+    public static final String UPDATE_SETTINGS = "INSERT INTO settings (usersId, music, sound) VALUES (:userId, :music, :sound) \n ON DUPLICATE KEY UPDATE music = values(music), sound = values(sound)";
+	public static final String UPDATE_SCORES = "INSERT INTO scores (usersId, level, score, date) VALUES (:userId, :level, :score, :date) \n ON DUPLICATE KEY UPDATE level = values(level), score = values(score), date = values(date)";
 }
