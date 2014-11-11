5632d4232498075fb7a51a2f24050ceadb0ad5fe
Trevi Awater <AwaterTrevi@Gmail.com>
2014-11-11 09:57:44 +0100

    Cleaning up

--- et-game/src/main/java/nl/dalthow/etaron/framework/XmlConverter.java
+++ et-game/src/main/java/nl/dalthow/etaron/framework/XmlConverter.java
@@ -1,57 +1,92 @@
+/**
+ * Etaron
+ *
+ *
+ * @Author Dalthow Game Studios 
+ * @Class XmlConverter.java
+ *
+ **/
+
 package nl.dalthow.etaron.framework;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
 
 public class XmlConverter 
 {
+	// Declaration
+	
 	private Marshaller marshaller;
 	private Unmarshaller unmarshaller;
  
-	public Marshaller getMarshaller() {
+	
+	// Getters
+	
+	public Marshaller getMarshaller() 
+	{
 		return marshaller;
 	}
- 
-	public void setMarshaller(Marshaller marshaller) {
-		this.marshaller = marshaller;
-	}
- 
-	public Unmarshaller getUnmarshaller() {
+
+	public Unmarshaller getUnmarshaller() 
+	{
 		return unmarshaller;
 	}
  
-	public void setUnmarshaller(Unmarshaller unmarshaller) {
+	
+	// Setters
+	
+	public void setMarshaller(Marshaller marshaller) 
+	{
+		this.marshaller = marshaller;
+	}
+ 
+	public void setUnmarshaller(Unmarshaller unmarshaller)
+	{
 		this.unmarshaller = unmarshaller;
 	}
  
-	public void convertFromObjectToXML(Object object, String filepath)
-		throws IOException {
- 
-		FileOutputStream os = null;
-		try {
-			os = new FileOutputStream(filepath);
-			getMarshaller().marshal(object, new StreamResult(os));
-		} finally {
-			if (os != null) {
-				os.close();
+	public void convertFromObjectToXML(KeyMap object, String path) throws IOException 
+	{
+		FileOutputStream outputStream = null;
+		
+		try 
+		{
+			outputStream = new FileOutputStream(path);
+			getMarshaller().marshal(object, new StreamResult(outputStream));
+		} 
+		
+		finally 
+		{
+			if(outputStream != null) 
+			{
+				outputStream.close();
 			}
 		}
 	}
  
-	public Object convertFromXMLToObject(String xmlfile) throws IOException {
- 
-		FileInputStream is = null;
-		try {
-			is = new FileInputStream(xmlfile);
-			return getUnmarshaller().unmarshal(new StreamSource(is));
-		} finally {
-			if (is != null) {
-				is.close();
+	public Object convertFromXMLToObject(String path) throws IOException 
+	{
+		FileInputStream inputStream = null;
+	
+		try 
+		{
+			inputStream = new FileInputStream(path);
+			
+			return getUnmarshaller().unmarshal(new StreamSource(inputStream));
+		} 
+		
+		finally 
+		{
+			if(inputStream != null) 
+			{
+				inputStream.close();
 			}
 		}
 	}
